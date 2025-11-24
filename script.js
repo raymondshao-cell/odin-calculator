a573f08704f3df7bfb26d6f393a0817d0d1c5fc2
Raymond Shao <243028283+raymondshao-cell@users.noreply.github.com>
2025-11-24 12:23:17 +1100

    Clicking on numbers updates num1 or num2 depending on whether an operator was clicked, clicking on clear resets num1 and num2 to 0 and resets operator to ''

--- script.js
+++ script.js
@@ -16,20 +16,20 @@ function divide(a,b) {
 }
 
 function operate(operator, a, b) {
-    if (operator === "+") {
+    if (operator === '+') {
         return add(a, b);
-    } else if (operator === "-") {
+    } else if (operator === '-') {
         return subtract(a,b);
-    } else if (operator === "x") {
+    } else if (operator === 'x') {
         return multiply(a,b);
-    }else if (operator === "/") {
+    }else if (operator === '/') {
         return divide(a,b);
     }
 }
 
-let num1 = 3;
-let num2 = 5;
-let operator = "";
+let num1 = 0;
+let num2 = 0;
+let operator = '';
 
 const layout = [
     ['7','8','9','Clear'],
@@ -51,18 +51,42 @@ layout.forEach(row => {
         const newKey = document.createElement('button');
         newKey.textContent = key;
 
-        if (['/', 'x', '-', '+', '='].includes(key)) {
+        if (['/', 'x', '-', '+'].includes(key)) {
             newKey.classList.add('operator');
+            newKey.addEventListener('click', () => {
+                if (num1 >= 0) {
+                    operator = `${newKey.textContent}`;
+                    console.log(operator);
+                }
+            })
+        } else if (['='].includes(key)) {
+            newKey.classList.add('equals');
+            newKey.addEventListener('click', () => {
+                display.textContent = `0`;
+                num1 = 0;
+                num2 = 0;
+                operator = '';
+            })
         } else if (['Clear'].includes(key)) {
             newKey.classList.add('clear');
             newKey.addEventListener('click', () => {
                 display.textContent = `0`;
+                num1 = 0;
+                num2 = 0;
+                operator = '';
             })
             
         } else {
             newKey.classList.add('digit');
             newKey.addEventListener('click', () => {
                 display.textContent = `${newKey.textContent}`;
+                if (operator === '') {
+                    num1 = parseInt(newKey.textContent);
+                    console.log(`num1: ${num1}`);
+                } else {
+                    num2 = parseInt(newKey.textContent);
+                    console.log(`num2: ${num2}`);
+                }
             })
         }
 
